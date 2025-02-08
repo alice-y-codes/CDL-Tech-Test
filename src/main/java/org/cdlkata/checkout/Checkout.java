cce0cf8918ce3780ec41d366b93bca9d5fbff107
alice-y-codes <alex.yang1115@gmail.com>
2025-02-08 13:48:30 +0000

    feat: add basic scan

--- src/main/java/org/cdlkata/checkout/Checkout.java
+++ src/main/java/org/cdlkata/checkout/Checkout.java
@@ -1,4 +1,15 @@
 package org.cdlkata.checkout;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
 public class Checkout {
+    private int total = 0;
+
+    public void scan(char item) {
+        if (item == 'A') {}
+        total += 50;
+    }
+
 }

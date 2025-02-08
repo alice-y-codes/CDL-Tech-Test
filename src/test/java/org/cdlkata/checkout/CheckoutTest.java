1687ed21769a1b65b14e6a1e957547c8501453d4
alice-y-codes <alex.yang1115@gmail.com>
2025-02-08 13:41:51 +0000

    test: add basic unit test for checkout

--- src/test/java/org/cdlkata/checkout/CheckoutTest.java
+++ src/test/java/org/cdlkata/checkout/CheckoutTest.java
@@ -1,7 +1,33 @@
 package org.cdlkata.checkout;
 
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class CheckoutTest {
+
+    private Checkout checkout;
+
+    @BeforeEach
+    void setUp() {
+        checkout = new Checkout();
+    }
+
+    @Test
+    void shouldStartWithZeroTotal() {
+        assertEquals(0, checkout.getTotal());
+    }
+
+    @Test
+    void shouldCalculateTotalForScannedItems() {
+
+        checkout.scan('A');
+        checkout.scan('A');
+        checkout.scan('A');
+
+        assertEquals(150, checkout.getTotal());
+    }
+
 
 }

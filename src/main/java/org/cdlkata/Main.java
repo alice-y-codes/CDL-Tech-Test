d40fd324bc3faa59325c7d25dac8f126df7efa39
alice-y-codes <alex.yang1115@gmail.com>
2025-02-08 18:07:03 +0000

    feat: functionality to run application

--- src/main/java/org/cdlkata/Main.java
+++ src/main/java/org/cdlkata/Main.java
@@ -1,7 +1,25 @@
 package org.cdlkata;
 
+import org.cdlkata.checkout.*;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+        Map<String, PriceModifier> priceModifiers = new HashMap<>();
+        priceModifiers.put("A", new PriceModifier(50, 130, 3));
+        priceModifiers.put("B", new PriceModifier(30, 45, 2));
+        priceModifiers.put("C", new PriceModifier(20, 0, 0));
+        priceModifiers.put("D", new PriceModifier(15, 0, 0));
+
+        PriceCalculator priceCalculator = new PriceCalculator(priceModifiers);
+        Basket basket = new Basket();
+        Checkout checkout = new Checkout(priceCalculator, basket);
+        Scanner scanner = new Scanner(System.in);
+        CheckoutCLI cli = new CheckoutCLI(checkout, scanner);
+
+        cli.start();
     }
 }

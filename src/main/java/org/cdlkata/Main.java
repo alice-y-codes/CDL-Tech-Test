cc5df5d693bebf587cfd87b50308c038525da8fc
alice-y-codes <alex.yang1115@gmail.com>
2025-02-08 18:45:59 +0000

    refactor: to use csv mapper instead of hashmap

--- src/main/java/org/cdlkata/Main.java
+++ src/main/java/org/cdlkata/Main.java
@@ -1,25 +1,30 @@
 package org.cdlkata;
 
 import org.cdlkata.checkout.*;
+import org.cdlkata.utils.PriceModifierCSVMapper;
 
+import java.io.IOException;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        Map<String, PriceModifier> priceModifiers = new HashMap<>();
-        priceModifiers.put("A", new PriceModifier(50, 130, 3));
-        priceModifiers.put("B", new PriceModifier(30, 45, 2));
-        priceModifiers.put("C", new PriceModifier(20, 0, 0));
-        priceModifiers.put("D", new PriceModifier(15, 0, 0));
+        try {
 
-        PriceCalculator priceCalculator = new PriceCalculator(priceModifiers);
-        Basket basket = new Basket();
-        Checkout checkout = new Checkout(priceCalculator, basket);
-        Scanner scanner = new Scanner(System.in);
-        CheckoutCLI cli = new CheckoutCLI(checkout, scanner);
+            Map<String, PriceModifier> priceModifiers = PriceModifierCSVMapper.mapCSVToPriceModifiers("src/main/resources/price-modifiers.csv");
 
-        cli.start();
+            PriceCalculator priceCalculator = new PriceCalculator(priceModifiers);
+            Basket basket = new Basket();
+            Checkout checkout = new Checkout(priceCalculator, basket);
+            Scanner scanner = new Scanner(System.in);
+            CheckoutCLI cli = new CheckoutCLI(checkout, scanner);
+
+            cli.start();
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
     }
 }

046ca94f4324b7888772ef8714cf3dba1a23a63b
alice-y-codes <alex.yang1115@gmail.com>
2025-02-08 13:58:40 +0000

    test: unit tests for pricing modifier

--- src/test/java/org/cdlkata/checkout/CheckoutTest.java
+++ src/test/java/org/cdlkata/checkout/CheckoutTest.java
@@ -29,5 +29,38 @@ class CheckoutTest {
         assertEquals(150, checkout.getTotal());
     }
 
+    @Test
+    void shouldApplySpecialPricingForMultipleItems() {
+        PriceModifier[] priceModifiers = new PriceModifier[] {
+                new PriceModifier('A', 50, 130, 3)
+        };
 
+        Checkout checkout = new Checkout(priceModifiers);
+
+        checkout.scan('A');
+        checkout.scan('A');
+        checkout.scan('A');
+
+        assertEquals(130, checkout.getTotal());
+    }
+
+    @Test
+    void shouldApplyMultiplePricingRules() {
+        PriceModifier[] priceModifiers = new PriceModifier[] {
+                new PriceModifier('A', 50, 130, 3),
+                new PriceModifier('B', 30, 45, 2),
+                new PriceModifier('C', 20, 0, 0),
+        };
+
+        Checkout checkout = new Checkout(priceModifiers);
+
+        checkout.scan('A');
+        checkout.scan('A');
+        checkout.scan('A');
+        checkout.scan('B');
+        checkout.scan('B');
+        checkout.scan('C');
+
+        assertEquals(195, checkout.getTotal());
+    }
 }

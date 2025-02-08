5636e653e689a93c09fc59f4bc5017feef0317db
alice-y-codes <alex.yang1115@gmail.com>
2025-02-08 17:32:39 +0000

    refactor(test): to include pricing calculator and new price modifiers

--- src/test/java/org/cdlkata/checkout/CheckoutTest.java
+++ src/test/java/org/cdlkata/checkout/CheckoutTest.java
@@ -3,7 +3,9 @@ package org.cdlkata.checkout;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -18,83 +20,94 @@ class CheckoutTest {
 
     @Test
     void shouldStartWithZeroTotal() {
-        checkout = new Checkout();
+        Map<String, PriceModifier> priceModifiers = new HashMap<>();
+        priceModifiers.put("A", new PriceModifier(50, 0, 0));
+        PriceCalculator priceCalculator = new PriceCalculator(priceModifiers);
+        Basket basket = new Basket();
+        checkout = new Checkout(priceCalculator, basket);
 
         assertEquals(0, checkout.getTotal());
     }
 
     @Test
     void shouldCalculateTotalForScannedItems() {
-        checkout = new Checkout(List.of( new PriceModifier('A', 50, 0, 0)));
+        Map<String, PriceModifier> priceModifiers = new HashMap<>();
+        priceModifiers.put("A", new PriceModifier(50, 0, 0));
+        PriceCalculator priceCalculator = new PriceCalculator(priceModifiers);
+        Basket basket = new Basket();
+        checkout = new Checkout(priceCalculator, basket);
 
-        checkout.scan('A');
-        checkout.scan('A');
-        checkout.scan('A');
+        checkout.scan("A");
+        checkout.scan("A");
+        checkout.scan("A");
 
         assertEquals(150, checkout.getTotal());
     }
 
     @Test
     void shouldApplySpecialPricingForMultipleItems() {
-        List<PriceModifier> priceModifiers = List.of(
-                new PriceModifier('A', 50, 130, 3)
-        );
+        Map<String, PriceModifier> priceModifiers = new HashMap<>();
+        priceModifiers.put("A", new PriceModifier(50, 130, 3));
+        PriceCalculator priceCalculator = new PriceCalculator(priceModifiers);
+        Basket basket = new Basket();
+        checkout = new Checkout(priceCalculator, basket);
 
-
-        Checkout checkout = new Checkout(priceModifiers);
-
-        checkout.scan('A');
-        checkout.scan('A');
-        checkout.scan('A');
+        checkout.scan("A");
+        checkout.scan("A");
+        checkout.scan("A");
 
         assertEquals(130, checkout.getTotal());
     }
 
     @Test
     void shouldApplyMultiplePricingModifiers() {
-        List<PriceModifier> priceModifiers = List.of (
-                new PriceModifier('A', 50, 130, 3),
-                new PriceModifier('B', 30, 45, 2),
-                new PriceModifier('C', 20, 0, 0)
-                );
+        Map<String, PriceModifier> priceModifiers = new HashMap<>();
+        priceModifiers.put("A", new PriceModifier(50, 130, 3));
+        priceModifiers.put("B", new PriceModifier(30, 45, 2));
+        priceModifiers.put("C", new PriceModifier(20, 10, 0));
+        PriceCalculator priceCalculator = new PriceCalculator(priceModifiers);
+        Basket basket = new Basket();
+        checkout = new Checkout(priceCalculator, basket);
 
-        Checkout checkout = new Checkout(priceModifiers);
-
-        checkout.scan('A');
-        checkout.scan('A');
-        checkout.scan('A');
-        checkout.scan('B');
-        checkout.scan('B');
-        checkout.scan('C');
+        checkout.scan("A");
+        checkout.scan("A");
+        checkout.scan("A");
+        checkout.scan("B");
+        checkout.scan("B");
+        checkout.scan("C");
 
         assertEquals(195, checkout.getTotal());
     }
 
     @Test
     void shouldHandleUnrecognisedItems() {
-        List<PriceModifier> priceModifiers = List.of(new PriceModifier('A', 50, 130, 3));
+        Map<String, PriceModifier> priceModifiers = new HashMap<>();
+        priceModifiers.put("A", new PriceModifier(50, 130, 3));
+        PriceCalculator priceCalculator = new PriceCalculator(priceModifiers);
+        Basket basket = new Basket();
+        checkout = new Checkout(priceCalculator, basket);
 
-        Checkout checkout = new Checkout(priceModifiers);
-
-        checkout.scan('A');
-        checkout.scan('A');
-        checkout.scan('A');
-        checkout.scan('Z');
+        checkout.scan("A");
+        checkout.scan("A");
+        checkout.scan("A");
+        checkout.scan("Z");
 
         assertEquals(130, checkout.getTotal());
     }
 
     @Test
     void shouldResetCheckoutForNewTransaction() {
-        List<PriceModifier> priceModifiers = List.of(new PriceModifier('A', 50, 130, 3));
+        Map<String, PriceModifier> priceModifiers = new HashMap<>();
+        priceModifiers.put("A", new PriceModifier(50, 130, 3));
+        PriceCalculator priceCalculator = new PriceCalculator(priceModifiers);
+        Basket basket = new Basket();
+        checkout = new Checkout(priceCalculator, basket);
 
-        Checkout checkout = new Checkout(priceModifiers);
-
-        checkout.scan('A');
-        checkout.scan('A');
-        checkout.scan('A');
+        checkout.scan("A");
+        checkout.scan("A");
+        checkout.scan("A");
         checkout.reset();
-        checkout.scan('Z');
+        checkout.scan("Z");
 
         assertEquals(0, checkout.getTotal());
     }
